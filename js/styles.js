b5986abbaaf35ded439fb9286fe9a1e8fdd78a09
Allie Sudholt <allison.sudholt@gmail.com>
2016-04-25 13:55:09 -0500

    added some hover animation, still needs some tlc

--- js/styles.js
+++ js/styles.js
@@ -2,13 +2,13 @@ $(function() {
 
   $('.title').hover(
       function() {
-          var $this = $(this); // caching $(this)
+          var $this = $(this);
           $this.data('initialText', $this.text());
           $this.text("HEY Y'ALL!");
       },
 
       function() {
-          var $this = $(this); // caching $(this)
+          var $this = $(this);
           $this.text($this.data('initialText'));
       }
   );
