97594e2e37569f516646e487109e971fccd050a9
Allie Sudholt <allison.sudholt@gmail.com>
2016-04-25 19:14:28 -0500

    fixed right margin and manatee animation

--- js/styles.js
+++ js/styles.js
@@ -2,13 +2,13 @@ $(function() {
 
   $('.title').hover(
       function() {
-          var $this = $(this);
+          var $this = $(this); // caching $(this)
           $this.data('initialText', $this.text());
           $this.text("HEY Y'ALL!");
       },
 
       function() {
-          var $this = $(this);
+          var $this = $(this); // caching $(this)
           $this.text($this.data('initialText'));
       }
   );
@@ -16,3 +16,17 @@ $(function() {
   $('.resume').css('height', $(window).height()+'px');
 
 });
+
+  function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: {lat: 30.2672, lng: -97.7431}
+    });
+
+    var image = 'images/mahface.png';
+    var beachMarker = new google.maps.Marker({
+      position: {lat: 30.27415, lng: -97.73996},
+      map: map,
+      icon: image
+    });
+  }
